7f45b774b85cade1cfcf14456545ea2ea03d9c79
Teo_You <teoyou881@gmail.com>
2025-05-19 13:02:20 -0400

    first commit

---
+++ src/main/java/teo/spring_security/IndexController.java
@@ -0,0 +1,13 @@
+package teo.spring_security;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class IndexController {
+
+  @GetMapping("/")
+  public String index() {
+    return "index";
+  }
+}
