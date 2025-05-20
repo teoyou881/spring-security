827dacae4d3440904d123d41781906c22d6ffe10
Teo_You <teoyou881@gmail.com>
2025-05-20 12:13:42 -0400

    rememberMe

--- src/main/java/teo/spring_security/SecurityConfig.java
+++ src/main/java/teo/spring_security/SecurityConfig.java
@@ -2,8 +2,13 @@ package teo.spring_security;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.provisioning.InMemoryUserDetailsManager;
 import org.springframework.security.web.SecurityFilterChain;
 
 @EnableWebSecurity
@@ -13,7 +18,23 @@ public class SecurityConfig {
   @Bean
   public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
     http.authorizeHttpRequests(auth -> auth.anyRequest().authenticated())
-        .httpBasic(basic -> basic.authenticationEntryPoint(new CustomAuthenticationEntryPoint()));
+        .formLogin(Customizer.withDefaults())
+        .rememberMe(rememberMe->rememberMe
+            // .alwaysRemember(true)
+            .tokenValiditySeconds(3600)
+            .userDetailsService(userDetailsService())
+            .rememberMeParameter("remember")
+            .rememberMeCookieName("remember")
+            .key("security")
+    );
+
     return http.build();
+  }
+
+
+  @Bean
+  public UserDetailsService userDetailsService(){
+    UserDetails user = User.withUsername("user").password("{noop}123").roles("USER").build();
+    return new InMemoryUserDetailsManager(user);
   }
 }
