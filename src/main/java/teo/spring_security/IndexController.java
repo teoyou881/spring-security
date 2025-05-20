58ee71442138a37c3cd65b81350979a8986ba13a
Teo_You <teoyou881@gmail.com>
2025-05-19 22:31:42 -0400

    formLogin

--- src/main/java/teo/spring_security/IndexController.java
+++ src/main/java/teo/spring_security/IndexController.java
@@ -10,4 +10,19 @@ public class IndexController {
   public String index() {
     return "index";
   }
+
+  @GetMapping("/loginPage")
+  public String loginPage() {
+    return "loginPage";
+  }
+
+  @GetMapping("/home")
+  public String home() {
+    return "home";
+  }
+
+  @GetMapping("/failed")
+  public String failed() {
+    return "failed";
+  }
 }
