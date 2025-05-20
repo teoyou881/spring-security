ee469f52708853b80ad764d269d1a181bce79b34
Teo_You <teoyou881@gmail.com>
2025-05-19 22:33:29 -0400

    httpBasic

---
+++ src/main/java/teo/spring_security/CustomAuthenticationEntryPoint.java
@@ -0,0 +1,19 @@
+package teo.spring_security;
+
+import jakarta.servlet.ServletException;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import java.io.IOException;
+import org.springframework.http.HttpStatus;
+import org.springframework.security.core.AuthenticationException;
+import org.springframework.security.web.AuthenticationEntryPoint;
+
+public class CustomAuthenticationEntryPoint implements AuthenticationEntryPoint {
+
+  @Override
+  public void commence(HttpServletRequest request, HttpServletResponse response, AuthenticationException authException)
+      throws IOException, ServletException {
+    response.setHeader("WWW-Authenticate", "Basic realm=Spring Security");
+    response.sendError(HttpStatus.UNAUTHORIZED.value(), HttpStatus.UNAUTHORIZED.getReasonPhrase());
+  }
+}
--- src/main/java/teo/spring_security/SecurityConfig.java
+++ src/main/java/teo/spring_security/SecurityConfig.java
@@ -1,18 +1,10 @@
 package teo.spring_security;
 
-import jakarta.servlet.ServletException;
-import jakarta.servlet.http.HttpServletRequest;
-import jakarta.servlet.http.HttpServletResponse;
-import java.io.IOException;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
-import org.springframework.security.core.Authentication;
-import org.springframework.security.core.AuthenticationException;
 import org.springframework.security.web.SecurityFilterChain;
-import org.springframework.security.web.authentication.AuthenticationFailureHandler;
-import org.springframework.security.web.authentication.AuthenticationSuccessHandler;
 
 @EnableWebSecurity
 @Configuration
@@ -21,36 +13,7 @@ public class SecurityConfig {
   @Bean
   public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
     http.authorizeHttpRequests(auth -> auth.anyRequest().authenticated())
-        .formLogin(form -> form
-            // .loginPage("/loginPage")
-            .loginProcessingUrl("/loginProc")
-            .defaultSuccessUrl("/", true)
-            .failureUrl("/failed")
-            .usernameParameter("userId")
-            .passwordParameter("passwd")
-
-            //successHandler & failureHandler가 defaultSuccessUrl, failureUrl 보다 우선 작동하게 된다.
-            .successHandler(new AuthenticationSuccessHandler() {
-              @Override
-              public void onAuthenticationSuccess(HttpServletRequest request, HttpServletResponse response,
-                  Authentication authentication) throws IOException, ServletException {
-                System.out.println("authentication = " + authentication);
-                response.sendRedirect("/home");
-              }
-            })
-            .failureHandler(new AuthenticationFailureHandler() {
-              @Override
-              public void onAuthenticationFailure(HttpServletRequest request, HttpServletResponse response,
-                  AuthenticationException exception) throws IOException, ServletException {
-                System.out.println("exception = " + exception.getMessage());
-                response.sendRedirect("/login");
-              }
-            })
-
-            //failureUrl(), loginPage(), loginProcessingUrl()에 대한 URL에 모든 사용자의 접근을 허용함
-            .permitAll()
-        );
-
+        .httpBasic(basic -> basic.authenticationEntryPoint(new CustomAuthenticationEntryPoint()));
     return http.build();
   }
 }
